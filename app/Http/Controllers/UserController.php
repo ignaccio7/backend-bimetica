6e9fba95e0983d31b0dc984a772b8803394b7b3a
ignaccio7 <nacho260298@gmail.com>
2026-03-28 12:17:32 -0400

    fix: update dependency

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,9 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Inertia\Response;
-use Pest\Support\Str;
 
 class UserController extends Controller
 {
