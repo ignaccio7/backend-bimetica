d819267b34ce38d44668bc469ff5e190a768b6bf
ignaccio7 <nacho260298@gmail.com>
2026-03-28 20:05:55 -0400

    fix: response type in exception handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Inertia\Inertia;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
