40bcc768b505dbbab98633d6ce57489966cb0403
ignaccio7 <nacho260298@gmail.com>
2026-04-27 20:39:01 -0400

    fix: fix route

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/panel/",
     plugins: [
         laravel({
             input: ["resources/js/app.jsx", "resources/css/app.css"],
